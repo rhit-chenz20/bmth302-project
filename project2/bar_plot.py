7c503328730410e645afaa67ea0947d0753a3270
rhit-chenz20 <chenz20@rose-hulman.edu>
2023-06-05 01:16:04 -0400

    plot update

--- project2/bar_plot.py
+++ project2/bar_plot.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser(description='Plot CSV result')
 parser.add_argument('-fn', '--filenames', type=str, nargs="*")
 parser.add_argument('-o', '--outputFile', type=str)
 parser.add_argument('-t', '--topPercent', type=float)
+parser.add_argument('-c', '--column', type=str)
 args = parser.parse_args()
 
 op = [0.1, 0.4, 0.4]
@@ -18,7 +19,8 @@ op_i=0
 for filename in args.filenames:
     df = pd.read_csv(filename)
     # df_sorted = df.sort_values("Ave_fit", ascending = False).head(n = int(len(df.index)*args.topPercent))
-    df_sorted = df.sort_values("Sum_fit", ascending = False).head(n = int(len(df.index)*args.topPercent))
+    # df_sorted = df.sort_values("Sum_fit", ascending = False).head(n = int(len(df.index)*args.topPercent))
+    df_sorted = df.sort_values(args.column, ascending = False).head(n = int(len(df.index)*args.topPercent))
 
     su = [0] * len(df["Genome"][0].split(","))
     x_label = range(len(df["Genome"][0].split(",")))
@@ -27,7 +29,7 @@ for filename in args.filenames:
         for i in range(len(genome)):
             su[i] += genome[i]
     
-    su = list(map(lambda x: 100*x/int(len(df.index)/2),su))
+    su = list(map(lambda x: 100*x/int(len(df.index)*args.topPercent),su))
     # creating the bar plot
     plt.bar(x_label, su, alpha=0.2,label = "Length = "+str(len(df["Genome"][0].split(","))), edgecolor = "black")
     op_i+=1
--- project2/runmany.sh
+++ project2/runmany.sh
@@ -12,50 +12,51 @@ declare V=1
 declare T=0.25
 
 # learning model
+# for MS in 1 5 10
+# do
+#     for FIT in 2
+#     do
+#         for ML in 3 5 10
+#         do
+#             for SZ in 500
+#             do
+#                 for FITBASE in 0.5
+#                 do
+#                     let "count+=1"
+#                     echo "running $count"
+
+#                     python3 run.py -fs $SZ -ml $ML -ms $MS -fit $FIT -fitbase $FITBASE -fn "result/${date}/CSV/ml_${ML}_ms_${MS}_fit_${FIT}_${V}" &
+#                     if ((count>$max))
+#                     then
+#                         wait
+#                         let "count=0"
+#                     fi
+#                 done
+#             done
+#         done
+#     done
+# done              
+# echo "finished simulation"
+
 for MS in 1 5 10
 do
     for FIT in 2
     do
-        for ML in 3 5 10
+        for ML in 3
         do
-            for SZ in 500
+            for SZ in 100
             do
-                for FITBASE in 0.5
-                do
-                    let "count+=1"
-                    echo "running $count"
+                let "count+=1"
+                echo "running $count"
 
-                    python3 run.py -fs $SZ -ml $ML -ms $MS -fit $FIT -fitbase $FITBASE -fn "result/${date}/CSV/ml_${ML}_ms_${MS}_fit_${FIT}_${V}" &
-                    if ((count>$max))
-                    then
-                        wait
-                        let "count=0"
-                    fi
-                done
+                python3 bar_plot.py -c Ave_fit -fn result/${date}/CSV/ml_*_ms_${MS}_fit_${FIT}_${V}_models.csv -o result/${date}/Fig/ms_${MS}_fit_${FIT}_t_${T}_sort_sumfit.png -t $T &
+                python3 bar_plot.py -c Sum_fit -fn result/${date}/CSV/ml_*_ms_${MS}_fit_${FIT}_${V}_models.csv -o result/${date}/Fig/ms_${MS}_fit_${FIT}_t_${T}_sort_sumfit.png -t $T &
+                if ((count>$max))
+                then
+                    wait
+                    let "count=0"
+                fi
             done
         done
     done
-done              
-echo "finished simulation"
-
-# for MS in 1 5 10
-# do
-#     for FIT in 0 1 
-#     do
-#         for ML in 3
-#         do
-#             for SZ in 100
-#             do
-#                 let "count+=1"
-#                 echo "running $count"
-
-#                 python3 bar_plot.py -fn result/${date}/CSV/ml_*_ms_${MS}_fit_${FIT}_${V}_models.csv -o result/${date}/Fig/ms_${MS}_fit_${FIT}_t_${T}_sort_sumfit.png -t $T &
-#                 if ((count>$max))
-#                 then
-#                     wait
-#                     let "count=0"
-#                 fi
-#             done
-#         done
-#     done
-# done          
+done          
